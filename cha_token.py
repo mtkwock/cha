3ea885b3c8785cfc7cf75d430d2a6d288d9ebc81
mtkwock <m.t.kwock@gmail.com>
2018-06-01 10:30:21 -0700

    Adding token classes.

---
+++ cha_token.py
@@ -0,0 +1,48 @@
+class Token(object):
+  def __init__(self, value):
+    self._value = value
+
+  def GetValue(self):
+    return self._value
+
+class StringToken(Token):
+  """A defined string (not multiline).
+  Includes the beginning and ending quotation.
+  """
+  pass
+
+class MultilineString(Token):
+  """A part of a multiline string.
+  Can begin or end with triple quotes, but can be an entire line without.
+
+  e.g.:
+
+  '''abc
+    OR
+  abc
+    OR
+  abc'''
+  """
+  pass
+
+class NumberToken(Token):
+  """A number token, can be of various forms:
+  e.g. 0b10101010, 0x01afb, 123 -123 0.0052 122.2 
+  """
+  pass
+
+class SymbolToken(Token):
+  """A single symbol:
+  e.g. +, -, &, &=, "
+  """
+  pass
+
+
+class VariableToken(Token):
+  """Variable names for classes, functions, and so on."""
+  pass
+
+class ReservedWordToken(Token):
+  """Python words such as class, def, True, and False."""
+  pass
+
